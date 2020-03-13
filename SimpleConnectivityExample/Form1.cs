bba3333f446051a6c2b088d4cb61cfa2e7e859b9
Ross Ylitalo <ross.ylitalo@gmail.com>
2020-03-13 09:50:39 -0400

    initial commit -- following tutorial https://www.guru99.com/c-sharp-access-database.html simple form with a button using a connection string to connect to SudoKubed database in SQLEXPRESS via SQLConnection.

---
+++ SimpleConnectivityExample/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace SimpleConnectivityExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdConnect_Click(object sender, EventArgs e)
+        {
+            string strConnectionString;
+            SqlConnection cnn;
+            strConnectionString = @"Data Source=ROSS-COMPUTER\SQLEXPRESS; Database=SudoKubed; Integrated Security=true;";
+            cnn = new SqlConnection(strConnectionString);
+            cnn.Open();
+            MessageBox.Show($"Connection state: {cnn.State}");
+            cnn.Close();
+        }
+    }
+}
